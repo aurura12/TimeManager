7982a08c8f8b41dfb352d42a5d6719d8928c82d0
zhoujiaqi <2411351557@qq.com>
2026-05-25 17:52:09 +0800

    增加未同步的标识

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:time_manager/screens/profile_screen.dart';
+import '../providers/time_provider.dart';
 import 'home_screen.dart';
 import 'target_screen.dart';
 
@@ -10,8 +12,28 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
   int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused ||
+        state == AppLifecycleState.detached) {
+      context.read<TimeProvider>().onAppBackgrounded();
+    }
+  }
 
   static final List<Widget> _widgetOptions = <Widget>[
     const HomeScreen(),
